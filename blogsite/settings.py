acb0af6c2f81830bef73cbacb21c883458167aef
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-07-03 23:25:21 +0100

    reverting back to prevous heroku code

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -28,12 +28,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = os.getenv(
-#   "ALLOWED_HOSTS",
-#    "127.0.0.1,localhost,https://threadly-bd86388a958b.herokuapp.com/"
-#).split(",")
-
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv(
+   "ALLOWED_HOSTS",
+    "127.0.0.1,localhost,https://threadly-bd86388a958b.herokuapp.com/"
+).split(",")
 
 # Application definition
 
