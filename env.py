d6d676fc79d9f771f5dadd394b4cee215cc5f0d8
Robert Lewis <robertlewis@Roberts-MacBook-Air.local>
2025-09-25 09:28:02 +0100

    adding in browser catagory

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "postgresql://neondb_owner:npg_1bqXJaxwMN9C@ep-falling-rice-a2ijfwz6.eu-central-1.aws.neon.tech/sneer_scarf_deck_719944", "<postgresql://neondb_owner:npg_1bqXJaxwMN9C@ep-falling-rice-a2ijfwz6.eu-central-1.aws.neon.tech/sneer_scarf_deck_719944>")
+
